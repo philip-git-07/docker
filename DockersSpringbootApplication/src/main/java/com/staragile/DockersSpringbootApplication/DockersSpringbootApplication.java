89776103527444a3bde80211f407461ca2e27626
philip-git-07 <141561502+philip-git-07@users.noreply.github.com>
2023-09-08 21:31:11 +0530

    Add files via upload

---
+++ DockersSpringbootApplication/src/main/java/com/staragile/DockersSpringbootApplication/DockersSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.staragile.DockersSpringbootApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockersSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockersSpringbootApplication.class, args);
+	}
+
+}
---
+++ DockersSpringbootApplication/src/test/java/com/staragile/DockersSpringbootApplication/DockersSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.staragile.DockersSpringbootApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockersSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
